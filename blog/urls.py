b429d7f9bcf726fd1dab306226c383eb3ca7bb1b
asifisthiaq24 <asifisthiaq24@gmail.com>
2019-10-10 19:12:19 +0600

    finished

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     url('^$', views.post_list, name='post_list'),
     url('^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url('^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url('^post/new/$', views.post_new, name='post_new'),
+    url('drafts/', views.post_draft_list, name='post_draft_list'),
+    url('post/(?P<pk>[0-9]+)/publish/', views.post_publish, name='post_publish'),
+    url('post/(?P<pk>[0-9]+)/remove/', views.post_remove, name='post_remove'),
 ]
