965e610198f5a2469496d7d1d30ed5999a8236fc
asifisthiaq24 <asifisthiaq24@gmail.com>
2019-10-13 11:35:38 +0600

    finished blog with login

--- DJ_1/urls.py
+++ DJ_1/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.conf.urls import url,include
+from django.contrib.auth import views
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
+    url('^admin/$', admin.site.urls),
+    url('^accounts/login/$', views.LoginView.as_view(), name='login'),
+    url('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     url('', include('blog.urls')),
 ]
