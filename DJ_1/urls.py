f039278f54f8e5def649d2b14f9bbfadd976951a
asifisthiaq24 <asifisthiaq24@gmail.com>
2019-10-13 11:54:31 +0600

    vul thik

--- DJ_1/urls.py
+++ DJ_1/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url,include
 from django.contrib.auth import views
 
 urlpatterns = [
-    url('^admin/$', admin.site.urls),
-    url('^accounts/login/$', views.LoginView.as_view(), name='login'),
+    url('admin/', admin.site.urls),
+    url('accounts/login/', views.LoginView.as_view(), name='login'),
     url('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     url('', include('blog.urls')),
 ]
