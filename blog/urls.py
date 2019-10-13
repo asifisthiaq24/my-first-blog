93010f555445543d2fb6f64ab41967374f49272e
asifisthiaq24 <asifisthiaq24@gmail.com>
2019-10-13 15:18:36 +0600

    a lot of changes

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     url('^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url('^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url('^post/new/$', views.post_new, name='post_new'),
-    url('drafts/', views.post_draft_list, name='post_draft_list'),
-    url('post/(?P<pk>[0-9]+)/publish/', views.post_publish, name='post_publish'),
-    url('post/(?P<pk>[0-9]+)/remove/', views.post_remove, name='post_remove'),
+    url('^drafts/$', views.post_draft_list, name='post_draft_list'),
+    url('^post/(?P<pk>[0-9]+)/publish/$', views.post_publish, name='post_publish'),
+    url('^post/(?P<pk>[0-9]+)/remove/$', views.post_remove, name='post_remove'),
+    url('^post/(?P<pk>[0-9]+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    url('^comment/(?P<pk>[0-9]+)/approve/$', views.comment_approve, name='comment_approve'),
+    url('^comment/(?P<pk>[0-9]+)/remove/$', views.comment_remove, name='comment_remove'),
 ]
