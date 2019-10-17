d248a6964142fc3c36683db79f7537f6adeaa008
asifisthiaq24 <asifisthiaq24@gmail.com>
2019-10-17 12:28:23 +0600

    post add fixed

--- blog/views.py
+++ blog/views.py
@@ -66,7 +66,7 @@ def add_comment_to_post(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = CommentForm()
-    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+    return render(request, 'blog/add_comment_to_post.html', {'form': form, 'pk':pk})
 
 @login_required
 def comment_approve(request, pk):
