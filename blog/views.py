54232de86539ea78c9598f5089e68e421176a9d5
asifisthiaq24 <asifisthiaq24@gmail.com>
2019-10-24 03:43:35 +0600

    data table data frame

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404,redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
 from django.utils import timezone
 from .forms import PostForm, CommentForm
@@ -21,20 +21,68 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+import numpy as np
+import pandas as pd
+import json
+
+def panda_testing():
+    df = pd.DataFrame({'A': 1.,
+                       'B': [211,211,1111,2],
+                       'C': pd.Series(1, index=list(range(4)), dtype='float32'),
+                       'D': np.array([3] * 4, dtype='int32'),
+                       'E': pd.Categorical(["test", "train", "test", "train"]),
+                       'F': 'foo'})
+    print 'printing data frame'
+    print df
+
+    ##
+    df_json_split = df.to_json(orient='split')
+    print df_json_split
+    df_json_records = df.to_json(orient='records')
+    print '\n\n\n\n'
+    print df_json_records
+    print 'sss'
+
+    formatted_obj = json.dumps(df_json_split, indent=4, separators=(',', ': '))
+    print(formatted_obj)
+
+    data_top = df.head(1) 
+    print '\n\n'
+    print data_top
+    print list(df.columns)
+    print df.get(list(df.columns))
+    for col in df.columns: 
+        print col
+
+
+# '{"columns": ["col 1", "col 2"],
+#   "index": ["row 1", "row 2"],
+#   "data": [["a", "b"], ["c", "d"]]}'
+##
+
+
 def home(request):
-    #db_operation()
+    # db_operation()
     return render(request, 'blog/home.html', {})
+
+
 @login_required
 def fifa_home(request):
+    panda_testing()
     return render(request, 'blog/fifa_main.html', {})
 
+
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
 @login_required
 def post_new(request):
     if request.method == "POST":
@@ -48,6 +96,8 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_new.html', {'form': form})
+
+
 @login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -61,21 +111,29 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form, 'pk':pk})
+    return render(request, 'blog/post_edit.html', {'form': form, 'pk': pk})
+
+
 @login_required
 def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+    posts = Post.objects.filter(
+        published_date__isnull=True).order_by('-created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
 @login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
+
+
 @login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
 
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -88,13 +146,15 @@ def add_comment_to_post(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = CommentForm()
-    return render(request, 'blog/add_comment_to_post.html', {'form': form, 'pk':pk})
+    return render(request, 'blog/add_comment_to_post.html', {'form': form, 'pk': pk})
+
 
 @login_required
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
     return redirect('post_detail', pk=comment.post.pk)
+
 
 @login_required
 def comment_remove(request, pk):
